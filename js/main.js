62899c6b6c4eafb289dde7c8a9653b903962d442
ada lovelace <williambookman@pursuit.org>
2023-11-19 23:59:51 -0500

    started adding functionality

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+const inputElement = document.querySelector(`[type="search box"]`)
+const searchButton = document.getElementById('search');
+const listOutput = document.getElementById('list-output');
+
+
+searchButton.addEventListener('click', () => {
+    const searchTerm = inputElement.value.trim();
+
+    if (!searchTerm) {
+        return;
+    }
+
+
+    // Make an API call to search for books using the search term
+    fetch('https://openlibrary.org/search.json?title=' + searchTerm)
+        .then(response => response.json())
+        .then(data => {
+            const docs = data.docs;
+
+            if (!docs.length) {
+                listOutput.innerHTML = '<p>No books found.</p>';
+                return;
+            }
+
+            let resultsHTML = '';
+            for (const doc of docs) {
+                resultsHTML += `
+                    <p>
+                    <strong>${doc.title_suggest}</strong><br>
+                    ${doc.author_name[0]}<br>
+                    <a href="${doc.covers?.[0]?.large}">Cover Image</a>
+                </p>`;
+            }
+
+            listOutput.innerHTML = resultsHTML;
+        });
+});
