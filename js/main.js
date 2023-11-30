96e3a6a24faa9153a897ff6b81cf64c3c10340a1
ada lovelace <williambookman@pursuit.org>
2023-11-30 06:07:29 -0500

    basis requirements completed

--- js/main.js
+++ js/main.js
@@ -1,41 +1,106 @@
+const bookDataURL =
+  "https://openlibrary.org/search.json?q=the+lord+of+the+rings&limit=3";
+const inputElement = document.querySelector(`[type="search box"]`); //selects search box
 
-const inputElement = document.querySelector(`[type="search box"]`) //selects search box
-const searchButton = document.getElementById('search'); //selects search button
-const listOutput = document.getElementById('list-output'); //selects the list of output of books
+console.log(inputElement);
+const searchButton = document.getElementById("search"); //selects search button
+const listOutput = document.getElementById("list-output"); //selects the list of output of books
+const searchResults = document.querySelector("h2");
 
+const searchTerm = inputElement.value.trim();
+console.log(searchTerm);
 
+fetch(bookDataURL + searchTerm)
+  .then((response) => response.json())
+  .then((data) => {
+    const books = data.docs;
+    console.log(books);
 
+    let resultsHTML = "";
+    for (const book of books) {
+      const bookTitle = book.title;
 
-searchButton.addEventListener('click', (event) => {
-    event.preventDefault()
-    const searchTerm = inputElement.value.trim();
+      const bookAuthor = book.author_name;
+      resultsHTML += `
+                      <p>
+                      <strong>${bookTitle}</strong><br>
+                      ${bookAuthor || `Unknown Author`}<br>
+                  </p>`;
 
-    if (!searchTerm) {
-        return;
-    }
+      searchButton.addEventListener("click", (event) => {
+        event.preventDefault();
 
-    // Make an API call to search for books using the search term
-    fetch('https://openlibrary.org/search.json?subject=hermeticism&title' + searchTerm)
-        .then(response => response.json())
-        .then(data => {
-            const docs = data.docs;
-
-            if (!docs.length) {
-                listOutput.innerHTML = '<p>No books found.</p>';
-                return;
-            }
-
-            let resultsHTML = '';
-            for (const doc of docs) {
-                resultsHTML += `
-                    <p>
-                    <strong>${doc.title_suggest}</strong><br>
-                    ${doc.author_name || `Unknown Author`}<br>
-                    <a href="${doc.covers?.[0]?.large}">Cover Image</a>
-                </p>`;
-            }
-
-            listOutput.innerHTML = resultsHTML;
+        books.filter((book) => {
+          if (book.title.includes(searchTerm)) {
+            resultsHTML = `
+                      <p>
+                      <strong>${bookTitle}</strong><br>
+                      ${bookAuthor || `Unknown Author`}<br>
+                  </p>`;
+                  listOutput.innerHTML = resultsHTML
+            return;    
+          } else {
+            console.log("keep digging");
+          }
+       
         });
-});
+    
+      });
+    }
+    listOutput.innerHTML = resultsHTML;
+  })
+  .catch((error) => {
+    console.error("Error fetching questions:", error);
+  });
 
+// searchButton.addEventListener("click", (event) => {
+//   event.preventDefault();
+
+//   const searchTerm = inputElement.value.trim();
+
+//   if (!searchTerm) {
+//     alert('please enter a book');
+//   }
+
+//   fetch(bookDataURL + searchTerm);
+
+//   if (!books.length) {
+//     listOutput.innerHTML = "<p>No books found.</p>";
+//     return;
+//   }
+
+//   let resultsHTML = "";
+//   for (const doc of docs) {
+//     resultsHTML += `
+//                     <p>
+//                     <strong>${doc.title_suggest}</strong><br>
+//                     ${doc.author_name || `Unknown Author`}<br>
+//                 </p>`;
+//   }
+
+//   listOutput.innerHTML = resultsHTML;
+// });
+
+// const generateBooks = () => {};
+// searchResults.style.display = "none";
+
+// searchButton.addEventListener("click", (event) => {
+//   event.preventDefault();
+
+//   if (!searchTerm) {
+//     return;
+//   }
+
+//   if (!books.length) {
+//     listOutput.innerHTML = "<p>No books found.</p>";
+//     return;
+//   }
+
+//   for (const book of books) {
+//     if (searchTerm === book.title) {
+//         console.log('found a match')
+//     }
+
+//   }
+
+// });
